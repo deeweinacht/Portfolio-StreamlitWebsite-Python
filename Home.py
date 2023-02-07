4deb52e84d6af8f0a642f9a47939778ac6333808
Dee <dweinacht@workmail.com>
2023-02-07 09:11:14 -0400

    Create projects page and add content

--- Home.py
+++ Home.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.set_page_config(layout='wide')
+st.set_page_config(page_title='homepage', layout='wide')
 col1, col2 = st.columns(2)
+
 
 with col1:
     st.image('images/work in progress.jpg')
@@ -10,7 +11,7 @@ with col2:
     about_me = """
     Hello, I'm Dee, and I'm an aspiring Data Scientist. I have a passion for
     working with data to reveal powerful and impactful new insights. I'm driven
-    to solve problems and want to always be learning new things. 
+    to solve problems and to always be learning new things. 
     I have a B.Sc. in Computer Science and have developed my skills in data
     analysis, data cleaning/wrangling, machine learning, and data visualization.
     I primarily work with Python but I am also a competent Java programmer. 
---
+++ pages/Projects.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import os
+
+st.set_page_config(page_title='portfolio', layout='wide')
+data = pd.read_csv('data.csv')
+
+st.title('Projects')
+st.write('Check out some of my coding and data projects:')
+
+
+
+for index, row in data.iterrows():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image('images/' + row['image'])
+    with col2:
+        st.subheader(row['title'])
+        st.write(row['description'])
+        st.caption(row['url'])
