3935215f8219a8d37cb788d73f5b8a0f7679e712
Dee <dweinacht@workmail.com>
2023-03-29 11:59:58 -0300

    Improved write-up

--- Home.py
+++ Home.py
@@ -1,26 +1,32 @@
 import streamlit as st
 
 st.set_page_config(page_title='Dee Weinacht - Home', layout='wide')
+st.header('Dee Weinacht')
+
 col1, col2 = st.columns(2)
-
-
 with col1:
     st.image('images/work in progress.jpg')
 with col2:
-    st.title('Dee Weinacht')
+    st.write("##### Hi, I'm Dee, and I'm an aspiring Data Scientist. I'm driven "
+                 "to learn new things and to solve challenging problems.")
     about_me = """
-    Hello, I'm Dee, and I'm an aspiring Data Scientist. I have a passion for
-    working with data to reveal powerful and impactful new insights. I'm driven
-    to solve problems and to always be learning new things. 
-    I have a B.Sc. in Computer Science and have developed advanced skills in data
-    analysis, data cleaning/wrangling, machine learning, and data visualization.
-    I primarily work in Python but I am also a competent Java programmer. 
+    As a recent computer science graduate with a passion for data science I have dedicated myself to learning the 
+    latest technologies and techniques in the field, honing my skills through a combination of coursework, 
+    independent learning, and personal projects. In doing so I have developed advanced skills in data preprocessing, 
+    data analysis, data visualization, and machine learning. I primarily work in Python but I am also a competent Java 
+    programmer. 
+
+    I am a highly conscientious, dedicated individual who is committed to delivering high-quality work that provides 
+    valuable and actionable insights. I am a strong team player who enjoys collaborating with others to tackle complex 
+    problems. I am excited to bring my skills and enthusiasm for data science to a challenging and rewarding position in 
+    the field.
     """
     st.write(about_me)
 
 st.write("")
 
 site_about = """
-This site is to highlight some of my personal projects, please take some time to look around!
+This portfolio site includes several projects that demonstrate my skills and knowledge
+ in python and data science. Please take some time to take a look!
 """
 st.write(site_about)
